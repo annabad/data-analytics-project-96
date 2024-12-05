204b766782d0ef2013a7652adc40a655f86f1200
annabad <110689156+annabad@users.noreply.github.com>
2024-12-05 12:55:56 +0300

    Update files for step 3

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,13 +1,14 @@
 --Витрина для расчета расходов на рекламу по модели атрибуции Last Paid Click
 with tab1 as (
     select
-        *,
+        s.visitor_id, visit_date, source, medium, campaign, content,
+        lead_id, amount, created_at, closing_reason, learning_format, status_id,
         row_number()
             over (partition by s.visitor_id order by s.visit_date desc)
         as rn
     from sessions as s
     left join leads on s.visitor_id = leads.visitor_id
-where medium <> 'organic'
+	where s.medium <> 'organic'
 ),
 
 last_paid_click as (
@@ -23,7 +24,7 @@ select
     closing_reason,
     status_id
 from tab1
-where rn = 1 and visit_date < coalesce(created_at, '2023-07-01')
+where rn = 1 and date(visit_date) <= coalesce(created_at, '9999-12-12')
 order by
     amount desc nulls last,
     visit_date asc,
@@ -59,8 +60,10 @@ select
     lpc.utm_source,
     lpc.utm_medium,
     lpc.utm_campaign,
-    count(distinct lpc.visitor_id) as visitors_count,
-    count(lpc.lead_id) as leads_count,
+    count(lpc.visitor_id) as visitors_count,
+    count(lpc.visitor_id) filter (
+    	where lpc.visit_date < lpc.created_at
+    ) as leads_count,
     count(lpc.closing_reason) filter (
         where lpc.status_id = 142
     ) as purchases_count,
@@ -71,16 +74,16 @@ group by 1, 2, 3, 4
 
 select
 agr.visit_date,
+agr.visitors_count,
 agr.utm_source,
 agr.utm_medium,
 agr.utm_campaign,
-agr.visitors_count,
 ads.total_cost,
 agr.leads_count,
 agr.purchases_count,
 agr.revenue
 from aggregate_lpc as agr
-left join ads_tab as ads
+inner join ads_tab as ads
 on
     agr.utm_source = ads.utm_source and agr.utm_medium = ads.utm_medium
     and agr.utm_campaign = ads.utm_campaign
